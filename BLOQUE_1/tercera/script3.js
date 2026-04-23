289c845400de8d1691efc12779ad43300903aa6b
Ana Rivera <asrivera886@gmail.com>
2026-04-22 20:38:40 -0500

    Tercera clase

---
+++ BLOQUE_1/tercera/script3.js
@@ -0,0 +1,42 @@
+// 1. Declara una variable de cada tipo (Number, String, Boolean, null, undefined)
+//    y haz console.log del typeof de cada una
+
+// 2. Crea estas variables:
+//    - Tu nombre
+//    - Tu edad
+//    - Si estás estudiando actualmente (boolean)
+//    - Tu lenguaje favorito hasta ahora
+
+// 3. Imprime todo en UN SOLO console.log usando template literals, así:
+//    "Hola, soy [nombre], tengo [edad] años,
+//     ¿estoy estudiando? [boolean], mi lenguaje favorito es [lenguaje]"
+
+// 4. Prueba esto y dime qué imprime y por qué crees que pasa:
+let resultado = "5" + 3;
+console.log(resultado);
+console.log(typeof resultado);
+
+
+////////////////////////////////////////
+let pais = "Colombia";
+let peso = 48.5;
+let baila = true;
+let nulo = null;
+let unde;
+
+console.log(typeof pais);
+console.log(typeof peso);
+console.log(typeof baila);
+console.log(typeof nulo);
+console.log(typeof unde);
+
+////////////////////////////////////////
+
+//2. 
+let nombre = "ANA";
+let edad = 18;
+let estudio = true;
+let fav = "Java";
+
+console.log(`Hola, soy ${nombre}, tengo ${edad} años, ¿estoy estudiando? ${estudio}, mi lenguaje favorito es ${fav}`);
+
